ac3adc7321326a4a051b308a63d11d89c2c47fd5
Nguyen Dang Van <nguyendv1406>
2023-09-03 19:49:08 +0900

    add env

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const dotenv = require('dotenv');
+dotenv.config();
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
